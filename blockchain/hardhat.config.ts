8a6e59880027018912ec0525af960dae91d5bbed
vini464 <vini.dev2003@gmail.com>
2025-12-11 15:34:05 -0300

    using geth

--- blockchain/hardhat.config.ts
+++ blockchain/hardhat.config.ts
@@ -3,6 +3,12 @@ import "@nomicfoundation/hardhat-toolbox";
 
 const config: HardhatUserConfig = {
   solidity: "0.8.20",
+  networks: {
+    geth: {
+      url: "http://ip_no1:8545",
+      accounts: [PRIVATE_KEY],
+    },
+  },
 };
 
 export default config;
